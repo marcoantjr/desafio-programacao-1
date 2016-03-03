9c930656906c0e0c429be65436a56fb77cb63ee6
Marco Antonio Cunha <marcoantjr@gmail.com>
2016-03-03 10:17:24 -0300

    Layout fix.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+       :rememberable, :trackable, :validatable
 devise :omniauthable, :omniauth_providers => [:google_oauth2]         
 has_many :order
 
