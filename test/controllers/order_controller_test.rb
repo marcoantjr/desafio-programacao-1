0e370d3ff0d160ba8cec231e802957485166200f
Marco Antonio Cunha <marcoantjr@gmail.com>
2016-03-03 16:28:33 -0300

    Including tests.

--- test/controllers/order_controller_test.rb
+++ test/controllers/order_controller_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class OrderControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+
 end
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
+include ActionDispatch::TestProcess
+
 class OrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	fixtures(:orders)
+
+	test "should import file" do
+		file = fixture_file_upload("files/input.tab", "text/plain")
+		order = Order.first
+		order.import(file)
+
+		order_items = OrderItem.where(order_id: order.id)
+		assert_not_nil order_items
+
+		order_items_prices = order_items.map {|item| item["price"]}.reduce(0, :+)
+		assert_equal order.price, order_items_prices
+
+		assert_equal order.filename, file.original_filename
+	end
+
+
 end
