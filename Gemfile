ed260a6d779ecbbf3b57a08b5da6436c87a874e3
Marco Antonio Cunha <marcoantjr@gmail.com>
2016-03-08 13:12:04 -0300

    Adding respond_to and respond_with.

--- Gemfile
+++ Gemfile
@@ -20,6 +20,9 @@ gem 'omniauth-google-oauth2'
 # Bootstrap SASS
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.6'
 
+# Getting "respond_with" back
+gem 'responders', '~> 2.1', '>= 2.1.1'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,14 +3,19 @@ class OrdersController < ApplicationController
 	before_action :authenticate_user!
 	before_action :find_order, only: [:show, :destroy]
 
+	respond_to :html
+	respond_to :json, only: [:index, :show]
+
 	# Finds all Orders to show
 	def index
 		@orders = Order.all
+		respond_with @orders
 	end
 
 	# Finds OrderItems for desidred Order
 	def show
 		@order_items = OrderItem.where(order_id: @order.id)
+		respond_with @order
 	end
 
 	# Destroy desired Order
@@ -47,7 +52,7 @@ class OrdersController < ApplicationController
 			begin
 				@order = Order.find(params[:id])
 			rescue
-				flash[:error] = "Error: The Order with id #{params[:id]} do not exists."
+				flash[:error] = "Error: The Order with id #{params[:id]} does not exists."
 				redirect_to root_path
 			end
 		end
